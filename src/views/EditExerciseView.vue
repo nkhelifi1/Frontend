0d0eeb3d72033537659dac34ad10888ee78a5fdb
PuhlmannJulia <s0584638@htw-berlin.de>
2023-06-15 16:52:20 +0200

    Hinzufügen des Edit Views

---
+++ src/views/EditExerciseView.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="home">
+  <InputForm/>
+</div>
+</template>
+
+<script>
+import InputForm from "@/components/InputForm.vue";
+
+export default {
+  name: 'EditExerciseView',
+  components: {
+    InputForm
+  }
+}
+</script>
+
+
+<style scoped>
+
+</style>
