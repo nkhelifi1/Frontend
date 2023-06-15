25f7740c77d4abc6105f083e7d273c30b18b5b9e
PuhlmannJulia <s0584638@htw-berlin.de>
2023-06-15 15:57:15 +0200

    Trennung von Exercise View und Component

---
+++ src/components/AllExercises.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="card">
+    <div class="col" v-for="exercise in exercises" :key="exercise.id">
+      <div class="card-body">
+        <h5 class="card-title">{{ exercise.name }}</h5>
+        <p class="card-text">Kategorie: {{ exercise.category}} Muskel: {{ exercise.muscleGroup }} Gewicht: {{ exercise.weight }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default{
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Exercises',
+  data () {
+    return {
+      exercises: []
+    }
+  },
+  mounted() {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    fetch('http://localhost:8080/api/exercises', requestOptions)
+        .then(response => response.json())
+        .then(result => result.forEach(exercise => {
+          this.exercises.push(exercise)
+        }))
+        .catch(error => console.log('error', error));
+  }
+}
+</script>
--- src/views/Exercise.vue
+++ src/views/Exercise.vue
@@ -1,35 +1,18 @@
 <template>
-  <div class="card">
-    <div class="col" v-for="exercise in exercises" :key="exercise.id">
-      <div class="card-body">
-        <h5 class="card-title">{{ exercise.name }}</h5>
-        <p class="card-text">Kategorie: {{ exercise.category}} Muskel: {{ exercise.muscleGroup }} Gewicht: {{ exercise.weight }}</p>
-      </div>
-    </div>
+  <div class="exercise">
+    <AllExercises/>
   </div>
 </template>
 
 <script>
-export default{
+// @ is an alias to /src
+import AllExercises from '@/components/AllExercises.vue'
+
+export default {
   // eslint-disable-next-line vue/multi-word-component-names
-  name: 'Exercises',
-  data () {
-    return {
-      exercises: []
-    }
-  },
-  mounted() {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    fetch('http://localhost:8080/api/exercises', requestOptions)
-        .then(response => response.json())
-        .then(result => result.forEach(exercise => {
-          this.exercises.push(exercise)
-        }))
-        .catch(error => console.log('error', error));
+  name: 'Exercise',
+  components: {
+    AllExercises
   }
 }
-
 </script>
