61a6e8fa7a48ff02af5e0a7aed3dc44a375acda3
PuhlmannJulia <s0584638@htw-berlin.de>
2023-06-15 16:08:05 +0200

    Navigationsbar mit Dropdown

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,15 +1,29 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">Exercise Tracking</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <router-link class="navbar-brand" to="/">Exercise Tracking</router-link>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-          <router-link class="nav-link" to="/">Home</router-link>
-          <router-link class="nav-link" to="/api/exercises">Exercises</router-link>
-        </div>
+      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link class="nav-link active" aria-current="page" to="/">Home</router-link>
+          </li>
+          <li class="nav-item dropdown">
+            <router-link class="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Übungen
+            </router-link>
+            <ul class="dropdown-menu">
+              <li><router-link class="dropdown-item" to="/api/exercises">Alle Übungen</router-link></li>
+              <li><a class="dropdown-item" href="#">Muskelgruppen</a></li>
+              <li><a class="dropdown-item" href="#">Push und Pull</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Übungen Bearbeiten</a>
+          </li>
+        </ul>
       </div>
     </div>
   </nav>
