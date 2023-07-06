dc78458a4734f7c17065d5d4559e501b0c5da5b1
PuhlmannJulia <s0584638@htw-berlin.de>
2023-07-06 11:08:34 +0200

    NavBar Test added

---
+++ tests/unit/navBar.spec.js
@@ -0,0 +1,23 @@
+import { shallowMount } from '@vue/test-utils';
+import Navbar from '@/components/Navbar.vue';
+
+describe('Navbar', () => {
+    it('renders navbar with correct elements and classes', () => {
+        const wrapper = shallowMount(Navbar);
+
+        const navbar = wrapper.find('nav');
+        expect(navbar.exists()).toBe(true);
+
+        const navbarBrand = wrapper.find('.navbar-brand');
+        expect(navbarBrand.exists()).toBe(true);
+        expect(navbarBrand.text()).toBe('curl.');
+
+        const navbarToggler = wrapper.find('.navbar-toggler');
+        expect(navbarToggler.exists()).toBe(true);
+
+        const navbarCollapse = wrapper.find('.navbar-collapse');
+        const navbarNav = navbarCollapse.find('.navbar-nav');
+        const navItems = navbarNav.findAll('.nav-item');
+        expect(navItems.length).toBe(3);
+    });
+});
