c77314c05814023399ea31c9c4e916bd8237c4e5
PuhlmannJulia <s0584638@htw-berlin.de>
2023-07-06 12:24:02 +0200

    Typo removed

--- tests/unit/navBar.spec.js
+++ tests/unit/navBar.spec.js
@@ -1,5 +1,5 @@
 import { shallowMount } from '@vue/test-utils';
-import Navbar from '@/components/Navbar.vue';
+import Navbar from '@/components/NavBar.vue';
 
 describe('Navbar', () => {
     it('renders navbar with correct elements and classes', () => {
