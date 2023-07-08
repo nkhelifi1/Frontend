05af23c9b2239a6c7c9ad44f58d4f8302f167f21
nkhelifi1 <khelifi.n@live.at>
2023-07-08 19:23:39 +0200

    "Übungen hinzufügen" Button new

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,39 +1,47 @@
 <template>
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-      <router-link class="navbar-brand text-blue" to="/">curl.</router-link>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <router-link class="nav-link active" aria-current="page" to="/">Home</router-link>
-          </li>
-          <li class="nav-item dropdown">
-            <router-link class="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Übungen
-            </router-link>
-            <ul class="dropdown-menu">
-              <li><router-link class="dropdown-item" to="/api/exercises">Alle Übungen</router-link></li>
-              <li><router-link class="dropdown-item" to="/api/exercisesGroup">Muskelgruppen</router-link></li>
-              <li><router-link class="dropdown-item" to="/api/exercisesCategory">Push und Pull</router-link></li>
-            </ul>
-          </li>
-          <li class="nav-item">
-            <router-link class="nav-link" to="/api/edit">Übungen hinzufügen</router-link>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <router-link class="navbar-brand text-blue" to="/">curl.</router-link>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <router-link class="nav-link active" aria-current="page" to="/">Home</router-link>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <router-link class="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Übungen
+                        </router-link>
+                        <ul class="dropdown-menu">
+                            <li><router-link class="dropdown-item" to="/api/exercises">Alle Übungen</router-link></li>
+                            <li><router-link class="dropdown-item" to="/api/exercisesGroup">Muskelgruppen</router-link></li>
+                            <li><router-link class="dropdown-item" to="/api/exercisesCategory">Push und Pull</router-link></li>
+                        </ul>
+                    </li>
+                </ul>
+                <div class="navbar-nav ml-auto">
+                    <div class="nav-item">
+                        <router-link class="btn btn-primary" style="background-color: #004aad;" to="/api/edit">Übungen hinzufügen</router-link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
 </template>
 
 <script>
 export default {
-  // eslint-disable-next-line vue/multi-word-component-names
-  name: 'Navbar'
-}
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'Navbar',
+    data() {
+        return {
+            filterCrit: ''
+        }
+    },
+
+    }
 </script>
 
 <style scoped>
